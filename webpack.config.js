59754f441fd5c9e8f0e3950ea3b198e15110de2d
wwitekk <wagabunda@gmail.com>
2017-11-22 23:32:27 +0100

    wip: initial setup - fixed issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
+
     context: path.resolve(__dirname, 'src'),
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'dist'),
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets:[
+                        ['es2015', {modules: false}]
+                    ]
+                }
+            }]
+        },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+    }
+
 }
